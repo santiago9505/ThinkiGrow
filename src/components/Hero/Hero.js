f34d46cccd370b111ca7817504bfd2a4242c3975
Juan Franco <jfrancoh5@gmail.com>
2021-09-12 00:13:07 -0500

    cambios al nav introduccion de hero

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,16 @@
+import React from 'react';
+//styles 
+import "../../assets/styles/Hero.css"
+
+const Hero = () =>{
+    return(
+        <section className="hero">
+            <h3>FINANCIA, ÚNETE E INVIERTE</h3>
+            <p className="hero__call-to-action">Únete a comunidades de aprendizaje, aporta a proyectos que cambien vidas,
+                o crea un proyecto al que todos quieran sumarse
+            </p>
+        </section>
+    );
+}
+
+export default Hero;
