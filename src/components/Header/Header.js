47df3d71ef14c1c1eb598d540eb5052b1b1c6e33
Santigeek <santigeek@hotmail.com>
2021-09-24 15:53:44 -0500

    getting done responsive until 1024px

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,6 +4,11 @@ import { Turn as Hamburger } from "hamburger-react";
 //styles
 import "../../assets/styles/Header.css";
 
+//images
+import Icon1 from "../../assets/static/menu-icon1.png";
+import Icon2 from "../../assets/static/menu-icon2.png";
+import Icon3 from "../../assets/static/menu-icon3.png";
+
 import { Link } from "react-router-dom";
 
 const Header = () => {
@@ -14,11 +19,12 @@ const Header = () => {
   };
 
   return (
-    <header className="header">
+    <header className={isOpen ? "header menu__open" : "header"}>
       <figure className="logo__container">
         <h1>THINKIGROW</h1>
       </figure>
       <Hamburger
+        className="menu-icon"
         onClick={handleClick}
         color="#fff"
         toggled={isOpen}
@@ -33,17 +39,17 @@ const Header = () => {
       >
         <li className="list__items--container">
           <a href="/">
-            REGISTRATE <img className="list__item--log-in-icon" />
+            Regístrate <img src={Icon1} className="list__item--log-in-icon" />
           </a>
         </li>
         <li className="list__items--container">
           <a href="/">
-            GRUPOS <img className="list__item--log-in-icon" />
+            Grupos <img src={Icon2} className="list__item--log-in-icon" />
           </a>
         </li>
         <li className="list__items--container">
           <a href="/">
-            CAMPAÑAS <img className="list__item--log-in-icon" />
+            Campañas <img src={Icon3} className="list__item--log-in-icon" />
           </a>
         </li>
         <li className="list__items--campaign">
